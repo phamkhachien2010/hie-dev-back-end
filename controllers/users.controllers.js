3fdacf4d9d52dc8ccb1f3cb73b146fc609ab3c7a
phamkhachien2010 <phamkhachien17@gmail.com>
2022-06-11 12:35:17 +0700

    fix bug upload img

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -67,7 +67,7 @@ const login = async(req, res) => {
 
 const uploadAvatar = async(req, res) => {
     const { file } = req;
-    const urlImage = `https://hie-dev.herokuapp.com/${file.path}`
+    const urlImage = `https://hie-dev.herokuapp.com/api/v1/${file.path}`
     const { user } = req;
     try {
         const userFound = await Users.findOne({
